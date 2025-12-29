e6c7db1ba150e687e09817190c17b1cf3f5c0de8
sonu kumar saini <167969764+sonukumar81075@users.noreply.github.com>
2025-12-29 15:18:08 +0530

    Update MongoDB connection URI to use Atlasmongod

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async () => {
-  const uri = "mongodb+srv://sonusaini:blog12345678@cluster0.v3qldvn.mongodb.net"
+  const uri = "mongodb+srv://sonusaini:blog12345678@cluster0.v3qldvn.mongodb.net/blogdb?retryWrites=true&w=majority"
   if (!uri) {
     throw new Error('MONGODB_URI is not set')
   }
